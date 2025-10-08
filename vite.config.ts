4c9257773507012d95550f6edf3dd59acdf5a338
Muhammad Ayub <138998122+Ayububi@users.noreply.github.com>
2025-10-08 14:46:42 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/m_ayub/'
+  base: '/m_ayub/',
 
   server: {
     host: "::",
